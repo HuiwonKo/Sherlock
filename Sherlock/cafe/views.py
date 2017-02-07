956f22a240b92a1590e90a62cb64158e3ed29cab
huiwon <huiwon@Huiwonui-MacBook-Pro.local>
2017-02-07 19:46:20 +0900

    Sherlock/cafe/views.py new,edit,delete 수정

--- Sherlock/cafe/views.py
+++ Sherlock/cafe/views.py
@@ -22,21 +22,41 @@ def room_detail(request,pk):
     review = get_object_or_404(Review, pk=pk)
     return render(request, "room_detail.html",{"review":review,})
 
-"""
+
 @login_required
-def review_new(request):
+def review_new(request,pk):
+
+    reviewObj = get_object_or_404(Review, pk=pk)
     if request.method == "POST":
         reviewForm = ReviewForm(request.POST)
         if reviewForm.is_valid():
             review = reviewForm.save(commit=False)
-
-
+            review.author = request.user
+            review.created_at = timezone.now()
+            review.save()
+            messages.success(request,"리뷰가 성공적으로 등록되었습니다.")
+            return redirect(room_detail, pk=pk)
+        else:
+            reviewForm = ReviewForm()
+        return render(request, 'room_detail.html',{'reviewForm':reviewForm , 'review':review})
 
 @login_required
-def review_edit(request):
-    pass
+def review_edit(request,pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        reviewForm = ReviewForm(request.POST, instance=review)
+        if reviewForm.is_valid():
+            review = reviewForm.save(commit=False)
+            review.author = request.user
+            review.save()
+            return redirect(room_detail, pk=pk)
 
 @login_required
-def review_delete(request):
-    pass
-"""
+def review_delete(request,pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.user != review.author:
+        messages.ERROR(request,"자신이 작성한 리뷰만 삭제할 수 있습니다.")
+        return redirect(room_detail, pk=pk)
+    review.delete()
+    messages.error(request,"리뷰가 삭제되었습니다.")
+    return redirect(room_detail)
