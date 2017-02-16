8a3e53c73b2fb64065ee1283c3dcd3a72cf77840
yuseon <yspre96@gmail.com>
2017-02-16 12:06:19 +0900

    배포

--- Sherlock/Sherlock/settings/prod.py
+++ Sherlock/Sherlock/settings/prod.py
@@ -7,10 +7,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'room_thumbnail')
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'yeonjunpark.mysql.pythonanywhere-services.com',
-        'NAME': 'yeonjunpark$sherlock',
-        'USER': 'yeonjunpark',
-        'PASSWORD': 'sherlock1',
+        'HOST': 'hyuseoni.mysql.pythonanywhere-services.com',
+        'NAME': 'hyuseoni$sherlock',
+        'USER': 'hyuseoni',
+        'PASSWORD': 'sherlock',
         'OPTIONS': {
             'sql_mode': 'traditional',  # strict mode
         },
