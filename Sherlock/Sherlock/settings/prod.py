b6f24d8017a96bc2c0815060aa400048fb008469
yuseon <yspre96@gmail.com>
2017-02-19 07:10:22 +0900

    마무리

--- Sherlock/Sherlock/settings/prod.py
+++ Sherlock/Sherlock/settings/prod.py
@@ -1,18 +1,18 @@
+import os
 from .common import *
 
 DEBUG == False
+ALLOWED_HOSTS = ['*']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'room_thumbnail')
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'hyuseoni.mysql.pythonanywhere-services.com',
-        'NAME': 'hyuseoni$sherlock',
-        'USER': 'hyuseoni',
-        'PASSWORD': 'sherlock',
-        'OPTIONS': {
-            'sql_mode': 'traditional',  # strict mode
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'NAME': 'ubuntu',
+        'USER': 'ubuntu',
+        'PASSWORD': 'sherlock.',
     },
 }
